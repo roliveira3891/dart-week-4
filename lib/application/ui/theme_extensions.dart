bf5969c8c67dc1a71cec6e26faf96e1d66ac9544
Renato Oliveira <roliveira3891@gmail.com>
2021-09-16 19:29:02 -0300

    Final Lesson 3

---
+++ lib/application/ui/theme_extensions.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+
+extension ThemeExtension on BuildContext {
+  Color get themeRed => const Color(0xFFEA4335);
+  Color get themeOrange => const Color(0xffFFBA49);
+  Color get themeGrey => const Color(0xff9B9B9B);
+}
--- lib/modules/splash/splash_controller.dart
+++ lib/modules/splash/splash_controller.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class SplashController extends GetxController {
-  @override
-  void onReady() {
-    super.onReady();
-    Get.offAllNamed('/login');
-  }
-}
+class SplashController extends GetxController {}
